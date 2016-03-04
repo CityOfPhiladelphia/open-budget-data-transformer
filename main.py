151434d441e7c1015101cbfba8038b0fadf870d4
timwis <tim@timwis.com>
2016-03-04 17:14:32 -0500

    working

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from openpyxl import load_workbook
 
+from classes import CLASSES
+
 FILE_NAME = './input/Obligation History.xlsx'
 SHEET_NAME = 'Sheet1'
 
@@ -13,20 +15,14 @@ def index_where(rows, column_index, needle):
       return row_index
   return -1
 
-# def concat_rows(rows):
-#   return [' '.join(filter(None, column)) for column in zip(*rows)]
-
-# Convert cells to strings
-rows = [[cell.value for cell in row] for row in rows]
-
 # Remove first 9 rows (sheet title)
 rows = rows[9:]
 
-# Remove column G and after
-# rows = [row[:6] for row in rows]
-
 # Only keep columns A and F
 rows = [[row[0]] + [row[5]] for row in rows]
+
+# Convert cells to strings
+rows = [[cell.value for cell in row] for row in rows]
 
 # Remove empty rows
 rows = [row for row in rows if any(row)]# and row[0] != 'Total']
@@ -35,12 +31,25 @@ rows = [row for row in rows if any(row)]# and row[0] != 'Total']
 first_row_to_remove_index = index_where(rows, 0, 'Total, General Fund')
 rows = rows[:first_row_to_remove_index]
 
-# Concatenate first 4 rows
-# header_row = concat_rows(rows[:4])
-# rows = [header_row] + rows[4:]
-
+# Convert "department sections" into database-friendly rows
+new_rows = [['Department', 'Class ID', 'Class', 'Total']]
+current_dept = ''
 for row in rows:
-  # print [cell.value for cell in row]
+  label = row[0]
+  total = row[1]
+
+  class_match = CLASSES.get(label)
+  if class_match:
+    # If it's a class, add a row to the new_rows array
+    new_rows.append([current_dept, class_match['id'], class_match['name'], total])
+  elif label == 'Total':
+    # When 'Total' row is reached, reset current_dept
+    current_dept = ''
+  else:
+    # Some departments span multiple lines
+    current_dept = ' '.join([current_dept.strip(), label.strip()])
+
+for row in new_rows:
   print row
-  # for cell in row:
-  #   print(cell.value)
+  
+# TODO: Check office of arts & culture (merged cells)
