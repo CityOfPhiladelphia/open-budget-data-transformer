f3b6164e8ceb82d1e0dca4f1c33535adb7b13890
timwis <tim@timwis.com>
2016-03-05 14:27:16 -0500

    other_funds working

--- other_funds.py
+++ other_funds.py
@@ -65,11 +65,12 @@ FUNDS = [
   },
 ]
 
-CLASSES = {
+CLASS_NAMES = {
   '100': 'Personal Services',
   '200': 'Purchase of Services',
   '300': 'Materials, Supplies & Equip.',
   '500': 'Contrib., Indemnities & Taxes',
+  '700': 'Debt Service',
   '800': 'Payments to Other Funds',
   '900': 'Advances & Miscellaneous Payments',
 }
@@ -77,6 +78,22 @@ CLASSES = {
 def float_or_zero(str):
   if str: return float(str)
   else: return 0
+
+def construct_dept_rows(dept, row):
+  dept_rows = []
+
+  if row[u'CLASS 100'] or row[u'PENSIONS'] or row[u'OTHER FB']:
+    total_class_100 = float_or_zero(row[u'CLASS 100']) + float_or_zero(row[u'PENSIONS']) + float_or_zero(row[u'OTHER FB'])
+    dept_rows.append([dept, 100, CLASS_NAMES['100'], total_class_100])
+  if row[u'CLASS 300'] or row[u'CLASS 400']:
+    total_class_300 = float_or_zero(row[u'CLASS 300']) + float_or_zero(row[u'CLASS 400'])
+    dept_rows.append([dept, 300, CLASS_NAMES['300'], total_class_300])
+
+  for key in ['200', '500', '700', '800', '900']:
+    if row.get(u'CLASS ' + key):
+      dept_rows.append([dept, key, CLASS_NAMES[key], row[u'CLASS ' + key]])
+
+  return dept_rows
 
 for fund in FUNDS:
   workbook = open_workbook(fund['input'])
@@ -88,31 +105,29 @@ for fund in FUNDS:
     rows.append(sheet.row_values(row_index))
 
   # Remove unneeded rows at top
-  rows = rows[6:]
+  rows = rows[4:]
 
   # Only keep columns A and C through K
-  rows = [[row[0]] + row[2:11] for row in rows]
+  rows = [[row[0]] + row[2:12] for row in rows]
 
   # Remove empty rows
   rows = [row for row in rows if any(row)]
+
+  # Convert list of lists to list of dicts
+  keys = rows[0]
+  rows = rows[1:]
+  rows = [dict(zip(keys, row)) for row in rows]
 
   # Convert "department sections" to individual department rows using FY17 amount
   new_rows = [['Department', 'Class ID', 'Class', 'Total']]
 
   current_dept = ''
   for row in rows:
-    label = row[0]
+    label = row[u'Department']
 
     if label.startswith('FY17'):
-      total_class_100 = float_or_zero(row[1]) + float_or_zero(row[2]) + float_or_zero(row[3])
-      total_class_300 = float_or_zero(row[5]) + float_or_zero(row[6])
-      if total_class_100: new_rows.append([current_dept, 100, CLASSES['100'], total_class_100])
-      if row[5]: new_rows.append([current_dept, 200, CLASSES['200'], row[5]])
-      if total_class_300: new_rows.append([current_dept, 300, CLASSES['300'], total_class_300])
-      if row[7]: new_rows.append([current_dept, 500, CLASSES['500'], row[7]])
-      if row[8]: new_rows.append([current_dept, 800, CLASSES['800'], row[8]])
-      if row[9]: new_rows.append([current_dept, 900, CLASSES['900'], row[9]])
-    elif not label.startswith('FY16') and not label.startswith('INCREASE'):
+      new_rows = new_rows + construct_dept_rows(current_dept, row)
+    elif not label.startswith('FY16') and not label.startswith('INCREASE') and not label.startswith('TOTAL'):
       current_dept = label
 
   with open(fund['output'], 'wb') as f:
