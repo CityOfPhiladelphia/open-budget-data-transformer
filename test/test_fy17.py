a43fb58e27d1eb4aaf138b07a500b66179cd4c43
timwis <tim@timwis.com>
2016-03-10 12:00:42 -0500

    add fy17 tests, test for 0 amounts, test number of records

--- test/test_fy17.py
+++ test/test_fy17.py
@@ -14,5 +14,9 @@ with open(DATA_FILE_PATH) as data_file:
 
 def test_fn():
   for test in tests:
-    results = [row for row in data_rows if all(row[key] == str(value) for key, value in test.items())]
-    yield nose.tools.assert_equals, len(results), 1, 'Could not find {0}'.format(test.values())
+    results = [row for row in data_rows if all(row[key] == str(value) for key, value in test.items() if key != 'Total')]
+    if test['Total'] == 0:
+      yield nose.tools.assert_equals, len(results), 0, 'Found {0} records for {1} expected 0'.format(len(results), test.values())
+    else:
+      yield nose.tools.assert_equals, len(results), 1, '{0} records for {1}'.format(len(results), test.values())
+      # yield.nose.tools.assert_equals, results[0], test['Total'], 'Incorrect total for {0}'.format(test.values())
