fc25b7b98a23973546a6220ed0f42178717655db
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2016-08-30 15:11:09 -0400

    use dictionaries when aggregating data for tests

--- FY17-adopted/test/test_fy17.py
+++ FY17-adopted/test/test_fy17.py
@@ -11,9 +11,11 @@ DATA_FILE_PATH = './output/FY17-adopted.csv'
 def aggregate (data, groupBy):
     grouped_dict = defaultdict(Decimal)
     for row in data:
-        # key = tuple(row[field] for field in groupBy)
-        key = (row['fund'], row['department'], row['class'])
-        grouped_dict[key] += int(row['total'])
+        key = tuple(row[field] for field in groupBy)
+        if key not in grouped_dict:
+            grouped_dict[key] = row.copy()
+            grouped_dict[key]['total'] = 0
+        grouped_dict[key]['total'] += int(row['total'])
     return grouped_dict
 
 with open(CONFIG_FILE_PATH) as config_file:
@@ -26,9 +28,10 @@ aggregated_data_rows = aggregate(data_rows, ['fund', 'department', 'class'])
 
 def test_fn():
   for test in tests:
-    results = [row for row in aggregated_data_rows if all(row[key] == str(value) for key, value in test.items() if key != 'total')]
+    results = [row for row in aggregated_data_rows.values() if all(row[key] == str(value) for key, value in test.items() if key != 'total')]
     if test['total'] == 0:
       yield nose.tools.assert_equals, len(results), 0, 'Found {0} records for {1} expected 0'.format(len(results), test.values())
     else:
       yield nose.tools.assert_equals, len(results), 1, '{0} records for {1}'.format(len(results), test.values())
-      yield nose.tools.assert_equals, results[0]['total'], str(test['total']), 'Incorrect total for {2}'.format(test['total'], results[0]['Total'], test.values())
+      results_total = results[0]['total'] if results else None
+      yield nose.tools.assert_equals, results_total, str(test['total']), 'Incorrect total for {2}: expected {0}, got {1}'.format(test['total'], results_total, test.values())
